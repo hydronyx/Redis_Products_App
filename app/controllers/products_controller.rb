05b30f7d3a351fb701195c1a9052d004504665d4
Suyog Nerkar <suyog.nerkar@weboniselab.com>
2014-02-26 12:50:48 +0530

    CRUD COMPLETE FOR REDIS APP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,17 @@
 class ProductsController < ApplicationController
+
   def index
-
-    @product_name='new'
-    @product_details=$redis.lrange("#{@product_name}",0,-1)
-
-
-    #@product_one=$redis.get("product_name")
-    respond_to do |format|
+   @product_name = [] 
+   @product_name = $redis.keys('*')
+   @product_details=$redis.lrange("#{@product_name}",0,-1)
+   respond_to do |format|
       format.html # index.html.erb
     end
   end
 
-  def new
-    @item = Product.new
 
+  def new
+    `echo  in new:#{@product_name} > /dev/pts/5`
     respond_to do |format|
       format.html # new.html.erb
     end
@@ -21,16 +19,37 @@ class ProductsController < ApplicationController
 
 
   def create
+    @product_name = params[:name]
+    $redis.lpush("#{@product_name}", "#{params[:price]}")
+    $redis.lpush("#{@product_name}", "#{params[:author]}")
+    $redis.lpush("#{@product_name}", "#{params[:marketer]}")
+
+    @product_details=$redis.lrange("#{@product_name}",0,-1)
+    render :action => 'show'
   end
 
   def show
+    # `echo  in show:#{@product_name} > /dev/pts/5`
+    @product_details=$redis.lrange("#{params[:id]}",0,-1)
   end
 
   def destroy
+    `echo  in show:#{params[:id]} > /dev/pts/3`
+    $redis.del("#{params[:id]}")
   end
 
   def update
+    @product_name = params[:id]
+    $redis.del("#{@product_name}")
+    $redis.lpush("#{@product_name}", "#{params[:price]}")
+    $redis.lpush("#{@product_name}", "#{params[:author]}")
+    $redis.lpush("#{@product_name}", "#{params[:marketer]}")
+
+    @product_details=$redis.lrange("#{@product_name}",0,-1)
+    render :action => 'show'
   end
 
+  def edit
+  end
   
 end
